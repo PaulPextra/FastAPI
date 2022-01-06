e544cad36a8c716b8084c0535d4b3e0e7f1131b2
Paul Okoli <paulokoli7@gmail.com>
2022-01-06 12:21:03 +0100

    Simple Blog FastAPI

--- main.py
+++ main.py
@@ -1,48 +1,17 @@
 from fastapi import FastAPI
-from typing import Optional
-from pydantic import BaseModel
+from blog import models
+from blog.database import engine
+from blog.routers import blog, user, authentication
 import uvicorn
 
 app = FastAPI()
 
-@app.get('/blogs')
-def index(limit=10, published : bool=True, sort : Optional[str] = None):
-    # only gets 10 published blogs
-    
-    if published:
-        return {'data':f'{limit} published Blog list from db'}
-    else:
-        return f'{limit} blog list from db'
+models.Base.metadata.create_all(engine)
 
-@app.get('/blog/unpublished')
-def unpublished():
-    # return unpublished blogs
-    
-    return {'data':'all unpublished blogs'}
-
-@app.get('/blog/{id}')
-def show(id : int):
-    # fetch blog with id = id
-    
-    return {'data': id}
-
-@app.get('/blog/{id}/comments')
-def comments(id, limit=10):
-    # fetch comments of blog with id = id
-
-    return {'data': ['1','2']}
-
-class Blog(BaseModel):
-    # Blog Model
-    
-    title : str
-    body : str
-    published : Optional[bool]
-    
-    
-@app.post('/blog')
-def create_blog(blog : Blog):
-    return {'data': blog}
+# Registering App Routers
+app.include_router(authentication.router)
+app.include_router(blog.router)
+app.include_router(user.router)
 
 
 # For Debuging Purpose
